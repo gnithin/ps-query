b39fee0bf9bf3dcddcd54d4533e75f4529b3f3db
Nithin <nithin.linkin@gmail.com>
2015-09-18 01:56:20 +0530

    Add ps_sqlify class

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 #!/user/bin/env python
 import subprocess
 
-def get_container_details():
-    '''
-    Run this command
-    docker ps -qa | xargs docker inspect
-    '''
-    ps_res = subprocess.Popen(
-                ["docker", "ps","-qa"],
-                stdout = subprocess.PIPE
-            )
+class ps_sqlify:
 
-    inspect_res = subprocess.Popen(
-                ["xargs", "docker","inspect"],
-                stdin = ps_res.stdout,
-                stdout = subprocess.PIPE
-            )
-    inspect_output, inspect_err_output = inspect_res.communicate()
+    @classmethod
+    def get_container_details(cls, args = "a"):
+        '''
+        Gets all the container details
+        '''
+        acceptable_args = "qal"
+        prefix_args = "q"
+        arg_list = ""
 
-    # TODO: Remove this block - Used for testing
-    #with open("example_output.out", 'w') as fp:
-    #    fp.write(inspect_output)
+        args = set(prefix_args + args)
 
-    return inspect_output
+        arg_list = '-' + ''.join([arg for arg in args if arg in acceptable_args])
+        
+        #Run this command
+        #docker ps -qa | xargs docker inspect
+        ps_res = subprocess.Popen(
+                    ["docker", "ps", arg_list],
+                    stdout = subprocess.PIPE
+                )
 
-def process_container_details(container_details):
-    return container_details
+        inspect_res = subprocess.Popen(
+                    ["xargs", "docker","inspect"],
+                    stdin = ps_res.stdout,
+                    stdout = subprocess.PIPE
+                )
+        inspect_output, inspect_err_output = inspect_res.communicate()
 
+        # TODO: Remove this block - Used for testing
+        with open("example_output.out", 'w') as fp:
+            fp.write(inspect_output)
+
+        return inspect_output
+
+    def process_container_details(cls, container_details):
+        return container_details
 
 if __name__ == "__main__":
-    container_details = get_container_details()
+    argument_list = "l"
+    container_details = ps_sqlify.get_container_details(argument_list)
