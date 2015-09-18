7ce901261df43e4e64b4f638e72d484ed1ab0da6
Nithin <nithin.linkin@gmail.com>
2015-09-19 02:00:45 +0530

    Create ps_sqlify class and return container data
    The ps_sqlify class has 2 private methods -
    __get_container_full_details which will simply give the output of the
    `docker inspect` command and __purge_container_details which will
    remove unnecessary data and keep only the ones that are required.
    ps_sqlify also has a public method `get_container_details` - that
    allows user to get the basic details regarding a container.
    purged output.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/user/bin/env python
 import subprocess
+import json
 
 class ps_sqlify:
-
     @classmethod
-    def get_container_details(cls, args = "a"):
+    def __get_container_full_details(cls, args = "a"):
         '''
         Gets all the container details
         '''
@@ -28,17 +28,71 @@ class ps_sqlify:
                     stdin = ps_res.stdout,
                     stdout = subprocess.PIPE
                 )
-        inspect_output, inspect_err_output = inspect_res.communicate()
+        inspect_output_str, inspect_err_output_str = inspect_res.communicate()
 
         # TODO: Remove this block - Used for testing
-        with open("example_output.out", 'w') as fp:
-            fp.write(inspect_output)
+        #with open("example_output.out", 'w') as fp:
+        #    fp.write(inspect_output)
+
+        inspect_output = json.loads(inspect_output_str) 
 
         return inspect_output
+    
+    @classmethod
+    def __purge_container_details(cls, container_details):
+        # Helper function #1
+        def get_keys(ip_dict, key):
+            dict_val = ip_dict
+            for k in key.split("."):
+                if type(dict_val) == dict and k in dict_val:
+                    dict_val = dict_val[k]
+                else:
+                    return None
+            return dict_val
+        
+        # Helper function #2
+        def get_safe_val(ip_dict, key, default=None):
+            val = get_keys(ip_dict, key)
+            return val if val != None else default
 
-    def process_container_details(cls, container_details):
-        return container_details
+        # Purge all the unnecessary fields
+        # The required_fields assumes docker inspect is done
+        # on a container id and not an Image id (That would give a different
+        # json structure output)
+        
+        required_fields = [
+            "Id", # container_id
+            "Image", # image_id
+            "Name", # container_name
+            "Config.Image", # image_name
+            "Config.Cmd", # command
+            "Created", # created
+            "State.StartedAt", #StartedAt
+            "State.FinishedAt", # FinishedAt
+            "State.Running", # status
+            "NetworkSettings.Ports"# ports
+        ]
+
+        new_container_details = []
+        
+        for cd in container_details:
+            t_store = {
+                    rf : get_safe_val(cd, rf) \
+                    for rf in required_fields \
+            }
+            new_container_details.append(t_store)
+        
+        return new_container_details
+
+    @classmethod
+    def get_container_details(cls, arg_list):
+        all_det = cls.__get_container_full_details(arg_list)
+        purged_det = cls.__purge_container_details(all_det)
+        return purged_det
+
 
 if __name__ == "__main__":
-    argument_list = "l"
-    container_details = ps_sqlify.get_container_details(argument_list)
+    arg_list = "l"
+    container_details = ps_sqlify.get_container_details(arg_list)
+    print json.dumps(container_details)
+
