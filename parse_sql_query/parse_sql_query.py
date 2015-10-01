1da790a3e0787fc2b37d14d6f420c6aea5ed1a3e
Nithin <nithin.linkin@gmail.com>
2015-10-02 04:04:26 +0530

    Add filtering logic to parse_sql_query

--- parse_sql_query/parse_sql_query.py
+++ parse_sql_query/parse_sql_query.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
-
-def parse_sql_query(query):
-    print query
-    return "4"
+from ply_files import parser
 
 
+def parse_sql_query(query, data=None):
+    # Call the parser.
+    # Get the response
+    # Filter out all the unwanted data elements.
+    parse_resp = parser(query, data=data)
+
+    resp_list = []
+    for i in xrange(len(data)):
+        if parse_resp[i] is True:
+            resp_list.append(data[i])
+
+    return resp_list
