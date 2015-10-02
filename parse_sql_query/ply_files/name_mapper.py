703b9839576f91a4f04cbf94adafde4f7e9ca908
Nithin <nithin.linkin@gmail.com>
2015-10-02 21:15:50 +0530

    Add map between query and data

---
+++ parse_sql_query/ply_files/name_mapper.py
@@ -0,0 +1,11 @@
+
+name_map = {
+        "image"        : "config.image",
+        "image_id"     : "image",
+        "container_id" : "Id",
+        "ports"        : "networkSettings.ports",
+        "running"      : "state.running",
+        "started_at"   : "state.startedat",
+        "finished_at"  : "state.finishedat",
+        "command"      : "config.cmd"
+}
