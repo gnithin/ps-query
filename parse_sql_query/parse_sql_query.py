c2fcdf98ee0099211668b0d71af33a978fefb0dc
Nithin <nithin.linkin@gmail.com>
2015-10-02 21:12:48 +0530

    Add data clean up

--- parse_sql_query/parse_sql_query.py
+++ parse_sql_query/parse_sql_query.py
@@ -2,11 +2,34 @@
 from ply_files import parser
 
 
+def clean_data(data):
+    # TODO: clean data(Do not change order)
+    new_data = []
+
+    for li_ele in data:
+        d = {}
+        for k, v in li_ele.iteritems():
+            # Add condition for data
+            # Key processing
+            d_key = k.lower()
+
+            # Value processing
+            if d_key == "name":
+                v = v.strip("/")
+
+            d[d_key] = v
+        new_data.append(d)
+
+    return new_data
+
+
 def parse_sql_query(query, data=None):
     # Call the parser.
     # Get the response
     # Filter out all the unwanted data elements.
-    parse_resp = parser(query, data=data)
+
+    cleaned_data = clean_data(data)
+    parse_resp = parser(query, data=cleaned_data)
 
     resp_list = []
     for i in xrange(len(data)):
