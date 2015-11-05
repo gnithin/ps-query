cbf401d11ece8ef71edba475313495039faad692
Nithin <nithin.linkin@gmail.com>
2015-11-06 01:00:45 +0530

    Add regex test

--- tests/test_basic_queries.py
+++ tests/test_basic_queries.py
@@ -1,11 +1,27 @@
 from ps_query import ps_query
 
 import unittest
+import re
 
 test_func = ps_query.get_container_details
 
+# TODO: Think about creating a list of containers
+# that would emulate the test properly.
+
 
 class TestPSQueryBasicFunctions(unittest.TestCase):
+    def test_regex(self):
+        regex = '.*(lee|lly).*'
+        query = ('name like "%s"' % regex)
+        arg = "a"
+        resp, status = test_func(
+            arg,
+            query
+        )
+
+        for e in resp:
+            regex_res = re.match(regex, e["Name"])
+            self.assertEqual(True, regex_res is not None)
 
     def test_name_equal(self):
         key = 'sleepy_einstein'
