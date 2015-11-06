951644482a5e474c5a718175262b42ad2005b11c
Nithin <nithin.linkin@gmail.com>
2015-11-06 23:49:15 +0530

    Add tests for datetime

--- tests/test_basic_queries.py
+++ tests/test_basic_queries.py
@@ -2,6 +2,7 @@ from ps_query import ps_query
 
 import unittest
 import re
+from dateutil.parser import parse as datetime_parser
 
 test_func = ps_query.get_container_details
 
@@ -9,7 +10,46 @@ test_func = ps_query.get_container_details
 # that would emulate the test properly.
 
 
+def get_secs(t):
+    '''
+    Helper function for converting string time into seconds
+    '''
+    return datetime_parser(t).strftime("%s")
+
+
 class TestPSQueryBasicFunctions(unittest.TestCase):
+    def test_date_1(self):
+        date_str = "2015-09-14 16:42:45"
+        conv_date_secs = get_secs(date_str)
+        query = ("created > '%s'" % date_str)
+        arg = "a"
+        resp, status = test_func(
+            arg,
+            query
+        )
+
+        # Asserting non-zero amount of responses.
+        self.assertGreater(len(resp), 0)
+        for e in resp:
+            # Convert to time
+            self.assertGreater(get_secs(e["Created"]), conv_date_secs)
+
+    def test_date_2(self):
+        date_str = "2015-09-14 16:42:45"
+        conv_date_secs = get_secs(date_str)
+        query = ("created = '%s'" % date_str)
+        arg = "a"
+        resp, status = test_func(
+            arg,
+            query
+        )
+
+        # Asserting non-zero amount of responses.
+        self.assertGreater(len(resp), 0)
+        for e in resp:
+            # Convert to time
+            self.assertEqual(get_secs(e["Created"]), conv_date_secs)
+
     def test_regex(self):
         regex = '.*(lee|lly).*'
         query = ('name like "%s"' % regex)
